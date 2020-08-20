983d2827f11728f7e82e9758432440717c2a448b
gorilla <log1230gori@outlook.jp>
2020-08-20 17:46:23 +0900

    commit

--- src/main/java/com/example/demo/trySpring/Employee.java
+++ src/main/java/com/example/demo/trySpring/Employee.java
@@ -1,11 +1,33 @@
 package com.example.demo.trySpring;
 
-import lombok.Data;
-
-@Data
 public class Employee {
 
     private int employeeId; //従業員ID
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
     private String employeeName; //従業員名
     private int age; //年齢
 }
